bb65ed3b2c7d60a1a68839cf3bd4b69e04373bbf
Jeffrey <sandford.jeffrey@gmail.com>
2018-12-15 21:59:00 +0000

    Do primitive error handling.
    If the user uploads a canceled purchase which we don't recognize, raise an
    UnknownSubscripionError; this is one of the "stretch goals". If we get an error
    at any stage of the databse transcation, rollback and proceed. Return all of the
    "failed" rows in a JSON document. I've done my best to capture any kind of
    400-class error, e.g. missing fields.

--- server.py
+++ server.py
@@ -3,7 +3,9 @@
 from contextlib import contextmanager
 from dateutil.parser import parse
 from flask import Flask, request, Response
+from json import dumps
 from mysql.connector import connect
+from sys import exc_info
 
 @contextmanager
 def subscriptions():
@@ -49,9 +51,17 @@ def update_product(db, row):
     cursor = db.cursor()
     cursor.execute(prod_insert, row[7:9])
 
+class UnknownSubscription(Exception):
+    """We cancelled a nonexistent subscription!"""
+    pass
+
+class UnrecognizedPurchase(Exception):
+    """Purchase status not one of: (canceled, new)!"""
+    pass
+
 def update_subscribers(db, row):
-    mode, cust, prod = row[6], row[0], row[7]
-    if mode == 'new':
+    status, cust, prod = row[6], row[0], row[7]
+    if status == 'new':
         sub_insert = '''
             INSERT INTO subscribers (
                 customer_id,
@@ -62,28 +72,55 @@ def update_subscribers(db, row):
                 (%s, %s, %s, %s);'''
         price, date = row[-2], parse(row[-1])
         db.cursor().execute(sub_insert, (cust, prod, date, price))
-    elif mode == 'canceled':
+    elif status == 'canceled':
         sub_update = '''
             DELETE FROM subscribers
             WHERE
                 customer_id = %s AND product_id = %s;'''
-        db.cursor().execute(sub_update, (cust, prod))
+        delete = db.cursor()
+        delete.execute(sub_update, (cust, prod))
+        if not delete.rowcount:
+            raise UnknownSubscription()
     else:
-        return Response(400)
+        raise UnrecognizedPurchase(status)
+
+class MissingOrderFields(Exception):
+    """We received an ambiguous input!"""
+    pass
 
 @app.route('/purchases', methods=['POST'])
 def update_subscriptions():
     with subscriptions() as db:
+        errors = []
         body = request.get_data(as_text=True)
+        i = 0
         for line in body.split('\n'):
+            i += 1
             if line:  # Trailing newline...
                 row = line.replace('\r', '').split('\t')
                 row = [col if col else None for col in row]  # No ''!
-                update_customer(db, row)
-                update_product(db, row)
-                update_subscribers(db, row)
-                db.commit()
-        return ('', 200)
+                try:
+                    if len(row) != 11:
+                        raise MissingOrderFields()
+                    update_customer(db, row)
+                    update_product(db, row)
+                    update_subscribers(db, row)
+                    db.commit()
+                except:
+                    db.rollback()
+                    e_type, _, __ = exc_info()
+                    errors.append({
+                        'line': i,
+                        'row': row,
+                        'err': e_type.__name__
+                        })
+        if not errors:
+            return ('', 200)
+        else:
+            return Response(
+                status=400,
+                content_type='application/json',
+                response=dumps(errors))
 
 if __name__ == '__main__':
     app.run(port=8080)
