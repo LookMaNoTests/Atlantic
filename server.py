79fb71d7e0e4988811646ccc604bd64726d70c4b
Jeffrey <sandford.jeffrey@gmail.com>
2018-12-15 20:23:42 +0000

    Add Flask application object.
    Passing __name__ like this is idiomatic.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """A simple Flask web server."""
 
 from contextlib import contextmanager
+from flask import Flask
 from mysql.connector import connect
 
 @contextmanager
@@ -16,4 +17,10 @@ def subscriptions():
     finally:
         if db:
             db.close()
+
+app = Flask(__name__)
+
+@app.route('/purchases', methods=['POST'])
+def update_subscriptions():
+    pass
 
